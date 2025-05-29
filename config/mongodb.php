bc02b341878f95f4edb936c2aca98d40e39de823
Aasawari <aasawari.sahasrabuddhe@mongodb.com>
2025-05-29 23:23:32 +0530

    Modifying database configurations

--- config/mongodb.php
+++ config/mongodb.php
@@ -1,13 +1,12 @@
 <?php
 
 return [
-
-    'driver'   => 'mongodb',
-    'dsn'      => env('MONGODB_URI', null),
-    'host'     => env('DB_HOST', '127.0.0.1'),
-    'port'     => env('DB_PORT', 27017),
-    'database' => env('DB_DATABASE', 'your_mongo_db'),
-    'username' => env('DB_USERNAME', ''),
-    'password' => env('DB_PASSWORD', '')
+    'default' => 'mongodb',
+    'connections' => [
+        'mongodb' => [
+            'driver'   => 'mongodb',
+            'dsn'      => env('MONGODB_URI'),
+            'database' => env('DB_DATABASE', 'test'),
+        ],
+    ],
 ];
-
--- src/MongoDBServiceProvider.php
+++ src/MongoDBServiceProvider.php
@@ -3,20 +3,30 @@
 namespace AasawariSahasrabuddhe\MongodbStarterKit;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Config;
 
 class MongoDbServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        // Merge the config with app config
-        $this->mergeConfigFrom(__DIR__.'/../config/mongodb.php', 'mongodb');
+        $this->mergeConfigFrom(__DIR__ . '/../config/mongodb.php', 'mongodb');
+
+        $mongodbConnections = Config::get('mongodb.connections');
+
+        if (is_array($mongodbConnections)) {
+            foreach ($mongodbConnections as $name => $connection) {
+                Config::set("database.connections.{$name}", $connection);
+            }
+        }
+        //setting to default
+        Config::set('database.default', Config::get('mongodb.default', 'mongodb'));
     }
 
     public function boot()
     {
-        // Allow publishing the config file
+        // Publish the package config to application's config directory
         $this->publishes([
-            __DIR__.'/../config/mongodb.php' => $this->app->basePath('config/mongodb.php'),
+            __DIR__ . '/../config/mongodb.php' => $this->app->basePath('config/mongodb.php'),
         ], 'config');
     }
 }
